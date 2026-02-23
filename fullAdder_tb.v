d8846a29a9281b3cb6dfdea7ae4b9db4705341ad
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-02-23 08:37:38 -0500

    updating to fix errors and missing connections

--- fullAdder_tb.v
+++ fullAdder_tb.v
@@ -2,7 +2,7 @@
 
 module fullAdder_tb();
     reg A_f, B_f, Cin_f;
-    wire S_f, Co_f;
+    wire S_f, Co_f,s1,c1,c2;
     
     fullAdder uut(A_f,B_f,Cin_f,S_f,Co_f);
     
@@ -20,7 +20,7 @@ module fullAdder_tb();
         #10 A_f = 1'b0;
         B_f = 1'b1;
         Cin_f = 1'b0;
-        #10 $display(" A=%b, B=%b, Ci=%b, S=%b, Co=%b \n", A_f,B_f,Ci_f,S_f,Co_f);
+        #10 $display(" A=%b, B=%b, Ci=%b, S=%b, Co=%b \n", A_f,B_f,Cin_f,S_f,Co_f);
         
         #10 A_f = 1'b0;
         B_f = 1'b1;
