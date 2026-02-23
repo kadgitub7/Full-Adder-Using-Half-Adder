77abd90a695cc5acc4266a2cc6fcaad47702068e
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-02-23 08:23:24 -0500

    changed full adder module for proper connections

--- fullAdder.v
+++ fullAdder.v
@@ -1,22 +1,15 @@
 `timescale 1ns / 1ps
+
 module fullAdder(
-
     input A_f,
-
     input B_f,
-
     input Cin_f,
-
     output S_f,
-
     output Co_f
-
     );
-
+    wire s1,c1,c2;
+    halfAdder h1(A_f,B_f,s1,c1);
+    halfAdder h2(s1,Cin_f,S_f,c2);
     
-
-    halfAdder h1(A_f,B_f,S_f,co_h);
-
-    halfAdder h2(S_)
-
+    assign Co_f = c2 | c1;
 endmodule
